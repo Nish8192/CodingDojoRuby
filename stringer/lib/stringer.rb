79b46dff56b51cac0e6fb85802b8a3c9361afd25
nish8192 <nish8192@gmail.com>
2017-03-14 13:51:54 -0400

    Finished Model Spec assignment in RailsTDD

--- stringer/lib/stringer.rb
+++ stringer/lib/stringer.rb
@@ -2,7 +2,6 @@ require "stringer/version"
 module Stringer
     def self.spacify *strings
         str = strings.join(' ')
-        str
     end
 
     def self.minify(string, max)
@@ -12,22 +11,18 @@ module Stringer
         else
             result = string[0...max]
             result += "..."
-            result
         end
     end
 
     def self.replacify(oStr, rStr, nStr)
         result = oStr.gsub(rStr, nStr)
-        result
     end
 
     def self.tokenize(str)
         result = str.split(' ')
-        result
     end
 
     def self.removify(str, rStr)
         result = str.gsub(" "+ rStr, "")
-        result
     end
 end
